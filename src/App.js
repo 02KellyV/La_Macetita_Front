706f9672d7c39c6cae7cb63fb6cf560f5b4c41dc
MarySerna <mary.serna.8903@gmail.com>
2020-06-10 19:01:16 -0500

    new_design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Contact from "./pages/contact/contact";
-import HomePage from "./pages/homePage/HomePage";
-import Menu from "./pages/menu/Menu";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Menu />
-            <HomePage />
-          </Route>
-          <Route path="/contact">
-            <Menu />
-            <Contact />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
